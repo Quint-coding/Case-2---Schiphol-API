4ac70d3bf4b8c48bf50a2fc27345ef70d67ac1ad
Quint-coding <kloq996@gmail.com>
2025-04-02 15:31:31 +0200

    chat all flights

--- Case 2 - schiphol API.py
+++ Case 2 - schiphol API.py
@@ -87,7 +87,7 @@ def vlucht3(df_filtered):
     possible_columns = [
         'scheduleDateTime', 'actualLandingTime', 'estimatedLandingTime',
         'prefixICAO',
-        'flightNumber', 'flightDirection', 'serviceType', 'terminal',
+        'flightNumber', 'flightDirection', 'serviceType',
         'baggage_belt', 'pier', 'gate', 'destination',
     ]
  
@@ -202,11 +202,18 @@ df = df.dropna(subset=["latitude_deg", "longitude_deg"])
 SCHIPHOL_LON = 4.763889
 SCHIPHOL_LAT = 52.308611
 
-def visualize_flights_from_schiphol(df, selected_time):
-    selected_flights = df[df["scheduleTime"] == selected_time].copy()
-    if selected_flights.empty:
-        st.warning(f"No flights found for the selected time: {selected_time}")
-        return
+def visualize_flights_from_schiphol(df):
+    # Option to show all flights or filter by time
+    show_all = st.checkbox("Show all flights", value=True)
+    
+    if show_all:
+        selected_flights = df.copy()
+    else:
+        selected_time = st.select_slider("Select Time", options=sorted(df["scheduleTime"].unique()))
+        selected_flights = df[df["scheduleTime"] == selected_time].copy()
+        if selected_flights.empty:
+            st.warning(f"No flights found for the selected time: {selected_time}")
+            return
 
     # Separate departing and arriving flights
     departing = selected_flights[selected_flights['flightDirection'] == 'D'].copy()
@@ -220,8 +227,8 @@ def visualize_flights_from_schiphol(df, selected_time):
         data=departing,
         get_source_position="from",
         get_target_position="to",
-        get_source_color=[0, 0, 255, 100],
-        get_target_color=[0, 255, 0, 100],
+        get_source_color=[0, 0, 255, 200],
+        get_target_color=[0, 255, 0, 200],
         auto_highlight=True,
         get_width=5,
         pickable=True,
@@ -235,8 +242,8 @@ def visualize_flights_from_schiphol(df, selected_time):
         data=arriving,
         get_source_position="from",
         get_target_position="to",
-        get_source_color=[0, 0, 255, 100],
-        get_target_color=[0, 255, 0, 100],
+        get_source_color=[0, 0, 255, 200],
+        get_target_color=[0, 255, 0, 200],
         auto_highlight=True,
         get_width=5,
         pickable=True,
@@ -254,13 +261,13 @@ def visualize_flights_from_schiphol(df, selected_time):
     tooltip = {
         "html": """
             <b>Flight Information</b><br>
-            <b>Time:</b> {scheduleTime}<br>
-            <b>Airline:</b> {prefixICAO}<br>
-            <b>Direction:</b> {flightDirection}<br>
-            <b>To / From:</b> {country_name}
+            <b>Time:</b> {{scheduleTime}}<br>
+            <b>Airline:</b> {{prefixICAO}} {{flightNumber}}<br>
+            <b>Direction:</b> {{flightDirection}}<br>
+            <b>Location:</b> {{longitude_deg}}, {{latitude_deg}}
         """,
         "style": {
-            "backgroundColor": "grey",
+            "backgroundColor": "steelblue",
             "color": "white",
             "fontFamily": "Arial"
         }
