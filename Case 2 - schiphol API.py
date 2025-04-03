df0c41646ca600deaa92c0c9f07b069d86bda7a5
Quint-coding <kloq996@gmail.com>
2025-04-03 15:08:36 +0200

    colors

--- Case 2 - schiphol API.py
+++ Case 2 - schiphol API.py
@@ -177,7 +177,7 @@ def vlucht4(dataframe):
  
     st.plotly_chart(fig)
 
-flight_direction_colors = {'D': 'royalblue', 'A': 'seagreen'}
+flight_direction_colors = {'D': '#65e892', 'A': '#354abf'}
 
 def vlucht_pertijdstip(dataframe):
     df_grouped = dataframe.groupby(['scheduleTime', 'flightDirection']).size().reset_index(name='count')
